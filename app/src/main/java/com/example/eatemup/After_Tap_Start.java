8b5f4af090468be816d8405c1fb056d067f51f6b
alhussain50 <alhussain@iut-dhaka.edu>
2019-03-23 13:05:04 +0600

    Start menu(Update) + New attribute after taping 'START' button

---
+++ app/src/main/java/com/example/eatemup/After_Tap_Start.java
@@ -0,0 +1,14 @@
+package com.example.eatemup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class After_Tap_Start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_after__tap__start);
+    }
+}
